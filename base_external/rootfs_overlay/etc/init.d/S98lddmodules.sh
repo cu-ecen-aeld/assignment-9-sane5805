8c8b9c9289bec2dcf208935267f8ef015fc2077c
Saurav Negi <sane5805@colorado.edu>
2023-10-23 13:31:02 -0600

    issue fixed

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -3,14 +3,14 @@
 case "$1" in 
 	start)
 		echo 'start scull and misc-modules'
-		/usr/bin/module_load faulty
-		/usr/bin/scull_load
+		module_load faulty
+		scull_load
 		modprobe hello
 		;;
 	stop)
 		echo 'Stop scull and misc-modules'
-		/usr/bin/module_unload faulty
-		/usr/bin/scull_unload
+		module_unload faulty
+		scull_unload
 		rmmod hello
 		;;
 	*)
