abe19aa0070b4771ec45635b05c11167c4d45619
Saurav Negi <112284209+sane5805@users.noreply.github.com>
2023-10-28 21:04:24 -0600

    Update ldd.mk

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-LDD_VERSION = cd6902fab614a5831235a83da32683a406dd3b25
+LDD_VERSION = 09d5ac5dbad78eeaedb6e0f31f79b1c08b4e970b
 LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-sane5805.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
